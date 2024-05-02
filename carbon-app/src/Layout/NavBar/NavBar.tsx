31cdefea3ee909c1455ec142bc5fed846f48d629
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-05-02 16:16:28 -0400

    starting point of navbar, adding in text and icons

---
+++ carbon-app/src/Layout/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import './NavBar.scss';
+import React from 'react';
+
+export default function NavBar() {
+
+  return ( 
+    <>
+      <nav className='carbon-navbar'>
+        <h1>Test</h1>
+      </nav>
+    </>
+  )
+}
