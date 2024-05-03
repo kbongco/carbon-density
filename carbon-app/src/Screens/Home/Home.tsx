fe7d980f1cc1a9b39f2100e0a4e4a6165ee11cf9
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-05-02 21:23:53 -0400

    starting poiint for home screen

---
+++ carbon-app/src/Screens/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+export default function Home() {
+  return ( 
+    <>
+      <section className='carbon-density-home-info'>
+        <p>
+          This is a test
+        </p>
+      </section>
+    </>
+  )
+}
