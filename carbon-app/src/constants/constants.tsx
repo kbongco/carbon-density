151a542a27e959d1b2e6252d822ed1a0c0f4357d
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-05-03 11:58:50 -0400

    added in constants for API calls

--- carbon-app/src/constants/constants.tsx
+++ carbon-app/src/constants/constants.tsx
@@ -1,1 +1,8 @@
 export const countries = ['England', 'Scotland', 'Wales'];
+
+export const carbonIntensityAPI = 'https://api.carbonintensity.org.uk/intensity';
+export const carbonIntensityRegional = 'https://api.carbonintensity.org.uk/regional';
+
+export const getRegionalLocationData = (country: string) => {
+  return `https://api.carbonintensity.org.uk/regional/${country}`
+}
