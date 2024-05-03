52ae984b106c95f43f23cd9e861edcdad3cbe45d
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-05-03 15:13:37 -0400

    added in chart for the energy sources

--- carbon-app/src/Components/Chart.tsx
+++ carbon-app/src/Components/Chart.tsx
@@ -6,6 +6,7 @@ import {
   Legend
 } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import './Chart.scss';
 
 ChartJS.register(
   ArcElement,
@@ -13,15 +14,37 @@ ChartJS.register(
   Legend);
 
 export default function Chart({ todayData }: any) {
-  console.log(todayData);
+  const dataNames = todayData?.generationData?.generationmix?.map((name: { fuel: string; }) => name.fuel || []);
+  const dataNumbers = todayData?.generationData?.generationmix?.map((percent: { perc: number }) => percent.perc);
+
+  const data = {
+    labels: dataNames,
+    datasets: [
+      {
+        data: dataNumbers,
+        backgroundColor: [
+          '#006400',
+          '#808080',
+          '#D3D3D3',
+          '#0000FF',
+          '#00008B',
+          '#90EE90',
+          '#00008B',
+          '#FFFF00',
+          '#ADD8E6'
+        ]
+      }
+    ]
+  }
+
+  // TODO figure out how to make chart responsive without having to adjust the height and figure out how to make this more reusable 
+
   return ( 
     <>
-      <h1>This is a test of the chart component</h1>
-      <div>
-        {/* <Pie 
+      <div className='carbon-intensity-energy-graph'>
+        <Pie 
           data={data}
-          options={options}
-        /> */}
+        />
       </div>
     </>
   )
--- carbon-app/src/Screens/Home/Home.tsx
+++ carbon-app/src/Screens/Home/Home.tsx
@@ -49,6 +49,7 @@ export default function Home() {
   }, []);
 
   console.log(todayData);
+  
 
   return ( 
     <>
